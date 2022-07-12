943756c918eeeded4b10a3322c37b856cc71eaa3
1940232240 陈明燕 <1940232240@cmy>
2022-07-12 21:20:24 +0800

    添加项目文件。

---
+++ LibraryMIS/DataSet1.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryMIS {
+    
+    
+    public partial class DataSet1 {
+    }
+}
+
+namespace LibraryMIS.DataSet1TableAdapters {
+    
+    
+    public partial class bookTableAdapter {
+    }
+}
