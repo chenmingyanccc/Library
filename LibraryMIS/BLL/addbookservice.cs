9800e1792ee4aac49ab18bcf86069bf8ddb8bc3b
1940232240 陈明燕 <1940232240@cmy>
2022-07-13 16:08:48 +0800

    开始

---
+++ LibraryMIS/BLL/addbookservice.cs
@@ -0,0 +1,18 @@
+﻿using LibraryMIS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace LibraryMIS.BLL
+{
+    class addbookservice
+    {
+        private SqlConnection oleConnection1 = null;
+        DataSet ds;
+    }
+}
